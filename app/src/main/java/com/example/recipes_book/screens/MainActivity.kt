eb89ca550c7d647fa08022f6bf266046a173a6bf
Andrew <vad30001@gmail.com>
2022-10-17 21:07:19 +0300

    Created navigation component

--- app/src/main/java/com/example/recipes_book/screens/MainActivity.kt
+++ app/src/main/java/com/example/recipes_book/screens/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.recipes_book
+package com.example.recipes_book.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recipes_book.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
