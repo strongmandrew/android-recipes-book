1de6a65401e698bee972da20683af2b4dc3d075a
Andrew <vad30001@gmail.com>
2022-10-18 10:55:18 +0300

    Migrated to support fragment manager

--- app/src/main/java/com/example/recipes_book/screens/MainActivity.kt
+++ app/src/main/java/com/example/recipes_book/screens/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.recipes_book.screens
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.recipes_book.R
 import com.example.recipes_book.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
@@ -21,5 +24,26 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
+        mActivityMainBinding.bottomNavigation.setOnItemSelectedListener {
+            lateinit var destinationFragment: Fragment
+            when (it.itemId) {
+
+                R.id.recipe_item -> {
+                    destinationFragment = MainFragment()
+
+                }
+
+                R.id.favourites_item -> {
+                    destinationFragment = FavoritesFragment()
+
+                }
+
+            }
+
+            supportFragmentManager.beginTransaction().replace(R.id.navigation_host, destinationFragment).commit()
+
+            true
+
+        }
     }
 }
