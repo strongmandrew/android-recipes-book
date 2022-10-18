0fb981fe226bf349e7cf65173f725beb0729a2e7
Andrew <vad30001@gmail.com>
2022-10-18 10:41:23 +0300

    Created bottom navigation layout

--- app/src/main/java/com/example/recipes_book/screens/MainActivity.kt
+++ app/src/main/java/com/example/recipes_book/screens/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.recipes_book.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.findNavController
 import com.example.recipes_book.R
+import com.example.recipes_book.databinding.ActivityMainBinding
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mActivityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mActivityMainBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
     }
 }
